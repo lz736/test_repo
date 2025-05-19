622d70799564111cb19151ae1a7625ef23c0b0c3
Qiang Li <3104977059@qq.com>
2025-05-19 23:46:43 +0800

    1-5题

---
+++ l_0419/15.py
@@ -0,0 +1,29 @@
+'''
+age = 18
+print("我的名字是%s,我的国籍是%s"%("小李","中国"))     #%s是站字符串  %d是站数字
+print("我今年：%d 岁" %age)
+
+print("www","baidu","com",sep=".")   #sep=表示分割
+print("hello",end="")
+print("world",end="\t")   #end=后面什么都没写直接连接下一行  \t表示中间空一段  \n表示输出结束换行
+print("python",end="\n")
+print("end")
+'''
+
+
+# password = input("请输入密码:")
+# print("您刚刚输入的密码是：",password)
+
+#a = 10
+# a = input("请输入：")
+# print(type(a))
+
+# print("输入了一个数字：%s"%a)
+
+a = int("123")
+print(type(a))
+b = a + 100
+print(b)
+
+c = int(input("请输c："))
+print("输入了一个数字：%d"%c)
